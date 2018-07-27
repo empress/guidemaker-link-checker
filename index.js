b30110b9972c2571522e9c77ada09b604e538b34
Chris Manson <chris@stonecircle.io>
2018-07-27 18:13:40 +0100

    initial working commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const { expect } = require('chai');
+const walkSync = require('walk-sync');
+const { extname } = require('path');
+const { inspect } = require('util');
+
+const {
+  findMarkdownLinks,
+  getBadRelativeUrlsForFile,
+  getBadLineBreaks,
+  getBadImageUrls,
+} = require('./helpers');
+
+function printBadLinks(badLinks) {
+  return inspect(badLinks, { depth: null });
+}
+
+
+module.exports = () => {
+  const paths = walkSync('guides')
+    .filter(filePath => extname(filePath) === '.md')
+    .map(filePath => `guides/${filePath}`);
+
+  /**
+   * Autogenerate some mocha tests
+   */
+  paths.forEach((filepath) => {
+    it(`processing ${filepath}`, function () {
+      const links = findMarkdownLinks(filepath);
+      const badLinks = getBadRelativeUrlsForFile({
+        filepath,
+        links,
+      });
+
+      expect(badLinks, printBadLinks(badLinks)).to.be.empty;
+
+      const badLineBreaks = getBadLineBreaks(filepath);
+      expect(badLineBreaks, printBadLinks(badLineBreaks)).to.be.empty;
+
+      const badImageLinks = getBadImageUrls({
+        filepath,
+        links,
+      });
+
+      expect(badImageLinks, printBadLinks(badImageLinks)).to.be.empty;
+    });
+  });
+};
+
+describe('check all links in markdown files', function () {
+
+});
